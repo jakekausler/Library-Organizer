3260c803facf044bf3eacfeb6b2aa4135b2b6c57
Jake Kausler <Jake Kausler>
2016-05-06 09:06:34 -0400

    Make database file

--- functions.php
+++ functions.php
@@ -1,24 +1,5 @@
 <?php
-	class Database {
-		private static $servername = 'localhost';
-		private static $username = 'jakekaus_root';
-		private static $password = 'Jake021f2f1!';
-		private static $dbname = 'jakekaus_library';
-		private static $db;
-		private $connection;
-		private function __construct() {
-			$this->connection = new mysqli(self::$servername,self::$username,self::$password,self::$dbname);
-		}
-		function __destruct() {
-			$this->connection->close();
-		}
-		public static function getConnection() {
-			if (self::$db==null) {
-				self::$db = new Database();
-			}
-			return self::$db->connection;
-		}
-	}
+	require 'database.php';
 	function getConnection()
 	{
 		return Database::getConnection();
