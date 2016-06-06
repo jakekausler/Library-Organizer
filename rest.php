4e54d6340e2e59149ad8485d9c2524117e191d21
Jake Kausler <Jake Kausler>
2016-06-06 18:04:07 -0400

    Improve fuzzy search

--- rest.php
+++ rest.php
@@ -11,7 +11,7 @@
 	$ins = 15;
 	$del = 10;
 
-	$stopwords = array("a","about","above","above","across","after","afterwards","again","against","all","almost","alone","along","already","also","although","always","am","among","amongst","amoungst","amount","an","and","another","any","anyhow","anyone","anything","anyway","anywhere","are","around","as","at","back","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","below","beside","besides","between","beyond","both","bottom","but","by","call","can","cannot","cant","co","con","could","couldnt","cry","de","describe","detail","do","done","down","due","during","each","eg","either","else","elsewhere","empty","enough","etc","even","ever","every","everyone","everything","everywhere","except","few","for","from","front","full","further","get","had","has","hasnt","have","he","hence","her","here","hereafter","hereby","herein","hereupon","hers","herself","him","himself","his","how","however","ie","if","in","inc","indeed","interest","into","is","it","its","itself","keep","last","latter","latterly","least","less","ltd","made","many","may","me","meanwhile","might","mill","mine","more","moreover","most","mostly","move","much","must","my","myself","name","namely","neither","never","nevertheless","next","no","nobody","none","noone","nor","not","nothing","now","nowhere","of","off","often","on","once","one","only","onto","or","other","others","otherwise","our","ours","ourselves","out","over","own","part","per","perhaps","please","put","rather","re","same","see","seem","seemed","seeming","seems","serious","several","she","should","show","side","since","sincere","so","some","somehow","someone","something","sometime","sometimes","somewhere","still","such","than","that","the","their","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","thereupon","these","they","thin","this","those","though","through","throughout","thru","thus","to","together","too","top","toward","towards","un","under","until","up","upon","us","very","via","was","we","well","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","with","within","without","would","yet","you","your","yours","yourself","yourselves","the");
+	$stopwords = array("a","about","above","above","across","after","afterwards","again","against","all","almost","alone","along","already","also","although","always","am","among","amongst","amoungst","amount","an","and","another","any","anyhow","anyone","anything","anyway","anywhere","are","around","as","at","back","be","became","because","become","becomes","becoming","been","before","beforehand","behind","being","below","beside","besides","between","beyond","both","bottom","but","by","call","can","cannot","cant","co","con","could","couldnt","cry","de","describe","detail","do","done","down","due","during","each","eg","either","else","elsewhere","empty","enough","etc","even","ever","every","everyone","everything","everywhere","except","few","for","from","front","full","further","get","had","has","hasnt","have","he","hence","her","here","hereafter","hereby","herein","hereupon","hers","herself","him","himself","his","how","however","ie","if","in","inc","indeed","interest","into","is","it","its","itself","keep","last","latter","latterly","least","less","ltd","made","many","may","me","meanwhile","might","mill","mine","more","moreover","most","mostly","move","much","must","my","myself","name","namely","neither","never","nevertheless","next","no","nobody","none","noone","nor","not","nothing","now","nowhere","of","off","often","on","once","only","onto","or","other","others","otherwise","our","ours","ourselves","out","over","own","part","per","perhaps","please","put","rather","re","same","see","seem","seemed","seeming","seems","serious","several","she","should","show","side","since","sincere","so","some","somehow","someone","something","sometime","sometimes","somewhere","still","such","than","that","the","their","them","themselves","then","thence","there","thereafter","thereby","therefore","therein","thereupon","these","they","thin","this","those","though","through","throughout","thru","thus","to","together","too","top","toward","towards","un","under","until","up","upon","us","very","via","was","we","well","were","what","whatever","when","whence","whenever","where","whereafter","whereas","whereby","wherein","whereupon","wherever","whether","which","while","whither","who","whoever","whole","whom","whose","why","will","with","within","without","would","yet","you","your","yours","yourself","yourselves","the");
 	$smallNumbers = array("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen");
 	$tens = array("", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety");
 	$scaleNumbers = array("", "thousand", "million", "billion");
@@ -34,7 +34,7 @@
 
 	switch ($method) {
 		case 'GET':
-			$searchMaterial = split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(".", " ", str_replace("\\", " ", str_replace("&", "and", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $request[0])))))))))))));
+			$searchMaterial = convert_numbers(split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(".", " ", str_replace("\\", " ", str_replace("+", " plus ", str_replace("!", "", str_replace("?", "", str_replace("&", " and ", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $request[0])))))))))))))))));
 			if (isset($request[1]) && is_numeric($request[1])) {
 				$searchDistance = floatval($request[1]);
 			}
@@ -50,6 +50,7 @@
 			if (isset($request[5]) && is_numeric($request[5])) {
 				$del = floatval($request[5]);
 			}
+			echo implode(' ', $searchMaterial);
 			$books = [];
 			$matches = [];
 			$notmatches = [];
@@ -78,34 +79,42 @@
 	}
 
 	function searchMatches($title, $subtitle, $material, $distance, $percent, $stopwords, $rep, $ins, $del, &$matches, &$notmatches) {
-		$titlearray = convert_numbers(split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(":", " ", str_replace(".", " ", str_replace("/", " ", str_replace("&", "and", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $title)))))))))))))));
-		$subtitlearray = convert_numbers(split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(":", " ", str_replace(".", " ", str_replace("/", " ", str_replace("&", "and", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $subtitle)))))))))))))));
+		$titlearray = convert_numbers(split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(":", " ", str_replace(".", " ", str_replace("/", " ", str_replace("+", " plus ", str_replace("!", "", str_replace("?", "", str_replace("&", " and ", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $title))))))))))))))))));
+		$subtitlearray = convert_numbers(split(" ", strtolower(str_replace("  ", " ", str_replace("  ", " ", str_replace(";", " ", str_replace(":", " ", str_replace(".", " ", str_replace("/", " ", str_replace("+", " plus ", str_replace("!", "", str_replace("?", "", str_replace("&", " and ", str_replace("-", " ", str_replace(",", "", str_replace(", ", " ", str_replace('"', '', str_replace("'", "", $subtitle))))))))))))))))));
 		$found = [];
 		foreach ($material as $key => $term) {
 			if (!in_array($term, $stopwords)) {
 				array_push($found, false);
+				echo '<ul>';
 				foreach ($titlearray as $k => $t) {
 					if (!in_array($t, $stopwords)) {
+						echo '<li>'.$t;
 						if (levenshtein($t, $term, $rep, $ins, $del) <= $distance) {
 							$found[$key] = true;
 							// $matches[$term.'-'.$t] = levenshtein($term, $t, $rep, $ins, $del);
+							echo ' matched</li></ul>';
 							break;
 						} else {
 							// $notmatches[$term.'-'.$t] = levenshtein($term, $t, $rep, $ins, $del);
 						}
+						echo '</li>';
 					}
 				}
 				foreach ($subtitlearray as $k => $t) {
 					if (!in_array($t, $stopwords)) {
+						echo '<li>'.$t;
 						if (levenshtein($t, $term, $rep, $ins, $del) <= $distance) {
 							$found[$key] = true;
 							// $matches[$term.'-'.$t] = levenshtein($term, $t, $rep, $ins, $del);
+							echo ' matched</li></ul>';
 							break;
 						} else {
 							// $notmatches[$term.'-'.$t] = levenshtein($term, $t, $rep, $ins, $del);
 						}
+						echo '</li>';
 					}
 				}
+				echo '</ul>';
 			}
 		}
 		$count = 0;
@@ -128,6 +137,7 @@
 				array_push($retval, $value);
 			}
 		}
+		echo implode(' ', $retval).'</br>';
 		return $retval;
 	}
 
